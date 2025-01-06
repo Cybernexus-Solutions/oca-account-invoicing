ebf005ef037a3721f4792853058034523619f4d8
Pete Snyder <cybernexus00@yahoo.com>
2025-01-06 14:47:57 -0500

    [IMP] partner_invoicing_mode, partner_invoicing_mode_at_shipping: pre-commit execution

---
+++ partner_invoicing_mode/models/res_partner.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    invoicing_mode = fields.Selection([("standard", "Standard")], default="standard")
+    invoicing_user_id = fields.Many2one(
+        "res.users",
+        string="Invoicing User",
+        help="User that will be used to invoice this partner.",
+    )
+    one_invoice_per_order = fields.Boolean(
+        default=False,
+        help="Do not group sale order into one invoice.",
+    )
+
+    @api.model
+    def _commercial_fields(self):
+        return super()._commercial_fields() + [
+            "invoicing_mode",
+            "one_invoice_per_order",
+        ]
